11f2bf856d0e3f2627697f898c73726fc004e754
Rehmat Shah <muskanjee2021r@gmail.com>
2024-05-30 17:39:55 +0530

    changes new

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export const metadata = {
       "Rehmat Shah",
   },
   description:
-    "A unique creative portfolio designed by CodeBucks with cutting-edge technologies like Next.js, Tailwind CSS, Three.js, and Framer Motion. Experience the art of modern web development firsthand. Checkout CodeBucks on youtube.",
+    "A unique creative portfolio designed by Rehmat Shah with cutting-edge technologies like Next.js, Tailwind CSS, Three.js, and Framer Motion.",
 };
 
 export default function RootLayout({ children }) {
